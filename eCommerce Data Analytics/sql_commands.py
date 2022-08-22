ac798b935f28e00f160fe0b31453ace0aadfa58e
aronnicksnts <aronnicksnts@gmail.com>
2022-08-22 20:02:07 +0800

    rename remove_category to fromTable_remove_id
    repurposed remove_category so it would work with other tables

--- eCommerce Data Analytics/sql_commands.py
+++ eCommerce Data Analytics/sql_commands.py
@@ -29,12 +29,23 @@ class SQL:
         except Error as e:
             print("Error: ", e)
 
-    def generate_random_number(self):
+    @staticmethod
+    def generate_random_number():
         return uuid.uuid1().int>>64
 
-    def generate_random_id(self):
+    @staticmethod
+    def generate_random_id():
         return str(uuid.uuid4())
 
+    @staticmethod
+    def combine_query(table_name, id, data):
+        query = f"UPDATE {table_name} SET "
+        for (key, value) in data.items():
+            if type(value) == int:
+                query += f"{key} = {value}, "
+            else:
+                query += f"{key} = '{value}', "
+        query = query[:-2] + f" WHERE {table_name}_id = {id}"
     def add_category(self, category_name: str, category_parent: int =  0):
         try: 
             if category_name == None:
@@ -61,32 +72,25 @@ class SQL:
             for key in data.keys():
                 if key not in ['category_name', 'category_parent', 'category_active']:
                     raise NameError(f"{key} is not an acceptable variable for modify category")
-            if 'category_parent' in data.keys():
-                if not type(data['category_parent']) == int:
-                    raise TypeError("category_parent is should be an integer")
+            if 'category_parent' in data.keys() and not type(data['category_parent']) == int:
+                raise TypeError("category_parent is should be an integer")
             if 'category_active' in data.keys() and not type(data['category_active']) == int:
                 raise TypeError("category_active is should be an integer")
             if 'category_name' in data.keys() and not type(data['category_name']) == str:
                 raise TypeError("category_name is should be a string")
 
-            query = "UPDATE CATEGORY SET "
-            for (key, value) in data.items():
-                if type(value) == int:
-                    query += f"{key} = {value}, "
-                else:
-                    query += f"{key} = '{value}', "
-            query = query[:-2] + f" WHERE category_id = {category_id}"
+            query = self.combine_query('category', category_id, data)
             self.cursor.execute(query)
             self.conn.commit()
         except Exception as e:
             print(e)
 
-    def remove_category(self, category_id):
+    def fromTable_remove_id(self, table_name, id):
         try:
-            if not self.fromTable_id_exists('category', category_id):
-                raise NameError(f"category_id {category_id} does not exist in the database")
+            if not self.fromTable_id_exists(table_name, id):
+                raise NameError(f"{table_name}_id {id} does not exist in the database")
             
-            query = f"UPDATE CATEGORY SET 'category_active' = 0 WHERE category_id = {category_id}"
+            query = f"UPDATE {table_name} SET '{table_name}_active' = 0 WHERE {table_name}_id = {id}"
             self.cursor.execute(query)
             self.conn.commit()
         except Exception as e:
@@ -107,7 +111,6 @@ class SQL:
         self.cursor.execute(query)
         return self.cursor.fetchall()[0][0]
 
-
     def getTable_from_id(self, table_name, id):
         try:
             query = f"SELECT * FROM {table_name} WHERE {table_name}_id = {id}"
@@ -118,3 +121,39 @@ class SQL:
             return table
         except Exception as e:
             print(e)
+
+    def add_user(self, user_id, user_name):
+        try:
+            if not self.fromTable_id_exists("user", user_id):
+                raise "user_id already exists"
+            
+            query = f"INSERT INTO user (user_id, user_name) VALUES ({user_id}, '{user_name}')"
+            self.cursor.execute(query)
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    #Only accepts user_name, user_active, user_spent, user_total_active_time
+    def modify_user(self, user_id, **data):
+        try:
+            if not self.fromTable_id_exists("user", user_id):
+                raise NameError(f'user_id {user_id} does not exist in the database')
+            for key in data.keys():
+                if key not in ['user_name', 'user_active', 'user_spent', 'user_total_active_time']:
+                    raise NameError(f"{key} is not an acceptable variable for modify category")
+            if 'user_name' in data.keys() and not type(data['user_name']) == str:
+                raise TypeError("user_name is not a string")
+            if 'user_active' in data.keys() and not type(data['user_active']) == bool:
+                raise TypeError("user_active is not a boolean")
+            if 'user_spent' in data.keys() and not type(data['user_spent']) == float:
+                raise TypeError("user_spent is not a float/double")
+            if 'user_total_active_time' in data.keys() and not type(data['user_total_active_time']) == int:
+                raise TypeError("user_total_active_time is not an integer")
+            
+            query = self.combine_query('user', user_id, data)
+            self.cursor.execute(query)
+            self.conn.commit()
+
+        except Exception as e:
+            print(e)
+
