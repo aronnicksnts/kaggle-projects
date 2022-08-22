b0127c8528b3187461d099a836f62ee34b687773
aronnicksnts <aronnicksnts@gmail.com>
2022-08-22 20:13:42 +0800

    new functions

--- eCommerce Data Analytics/sql_commands.py
+++ eCommerce Data Analytics/sql_commands.py
@@ -33,9 +33,11 @@ class SQL:
     def generate_random_number():
         return uuid.uuid1().int>>64
 
+
     @staticmethod
     def generate_random_id():
         return str(uuid.uuid4())
+
 
     @staticmethod
     def combine_query(table_name, id, data):
@@ -46,6 +48,49 @@ class SQL:
             else:
                 query += f"{key} = '{value}', "
         query = query[:-2] + f" WHERE {table_name}_id = {id}"
+
+
+    def fromTable_remove_id(self, table_name, id):
+        try:
+            if not self.fromTable_id_exists(table_name, id):
+                raise NameError(f"{table_name}_id {id} does not exist in the database")
+            
+            query = f"UPDATE {table_name} SET '{table_name}_active' = 0 WHERE {table_name}_id = {id}"
+            self.cursor.execute(query)
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+
+    def fromTable_name_exists(self, table_name, name) -> bool:
+        try:
+            query = f"SELECT EXISTS (SELECT * FROM {table_name} WHERE {table_name}_name = '{name}')"
+            self.cursor.execute(query)
+            return self.cursor.fetchall()[0][0]
+            
+
+        except Exception as e:
+            print(e)
+    
+
+    def fromTable_id_exists(self, table_name, id) -> bool:
+        query = f"SELECT EXISTS (SELECT * FROM {table_name} WHERE {table_name}_id = {id})"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()[0][0]
+
+
+    def getTable_from_id(self, table_name, id):
+        try:
+            query = f"SELECT * FROM {table_name} WHERE {table_name}_id = {id}"
+            self.cursor.execute(query)
+            table = self.cursor.fetchone()
+            if table is None:
+                return tuple()
+            return table
+        except Exception as e:
+            print(e)
+
+
     def add_category(self, category_name: str, category_parent: int =  0):
         try: 
             if category_name == None:
@@ -54,6 +99,8 @@ class SQL:
                 raise TypeError("category_name is not a string")
             elif type(category_parent) != int:
                 raise TypeError("category_parent is not an integer")
+            elif self.fromTable_name_exists('category', category_name):
+                raise "Category name already exists"
             category_id = self.generate_random_number()
             query = "INSERT INTO CATEGORY (category_id, category_parent, category_name) VALUES " \
             f"({category_id}, {category_parent}, '{category_name}');"
@@ -62,6 +109,7 @@ class SQL:
             print(f"Successfully added {category_name} into category")
         except Exception as e:
             print(e)
+
 
     #data can only include category_name, category_parent, category_active
     def modify_category(self, category_id, **data):
@@ -82,56 +130,26 @@ class SQL:
             query = self.combine_query('category', category_id, data)
             self.cursor.execute(query)
             self.conn.commit()
+
+            print("Sucessfully modified category")
         except Exception as e:
             print(e)
 
-    def fromTable_remove_id(self, table_name, id):
-        try:
-            if not self.fromTable_id_exists(table_name, id):
-                raise NameError(f"{table_name}_id {id} does not exist in the database")
-            
-            query = f"UPDATE {table_name} SET '{table_name}_active' = 0 WHERE {table_name}_id = {id}"
-            self.cursor.execute(query)
-            self.conn.commit()
-        except Exception as e:
-            print(e)
 
-    def fromTable_name_exists(self, table_name, name) -> bool:
-        try:
-            query = f"SELECT EXISTS (SELECT * FROM {table_name} WHERE {table_name}_name = '{name}')"
-            self.cursor.execute(query)
-            return self.cursor.fetchall()[0][0]
-            
-
-        except Exception as e:
-            print(e)
-    
-    def fromTable_id_exists(self, table_name, id) -> bool:
-        query = f"SELECT EXISTS (SELECT * FROM {table_name} WHERE {table_name}_id = {id})"
-        self.cursor.execute(query)
-        return self.cursor.fetchall()[0][0]
-
-    def getTable_from_id(self, table_name, id):
-        try:
-            query = f"SELECT * FROM {table_name} WHERE {table_name}_id = {id}"
-            self.cursor.execute(query)
-            table = self.cursor.fetchone()
-            if table is None:
-                return tuple()
-            return table
-        except Exception as e:
-            print(e)
-
-    def add_user(self, user_id, user_name):
+    def add_user(self, user_id, user_name = None):
         try:
             if not self.fromTable_id_exists("user", user_id):
                 raise "user_id already exists"
+            if user_id == None:
+                raise ValueError("No value for user_id found")
             
             query = f"INSERT INTO user (user_id, user_name) VALUES ({user_id}, '{user_name}')"
             self.cursor.execute(query)
             self.conn.commit()
+            print(f"Successfully added User {user_id}")
         except Exception as e:
             print(e)
+
 
     #Only accepts user_name, user_active, user_spent, user_total_active_time
     def modify_user(self, user_id, **data):
@@ -154,6 +172,69 @@ class SQL:
             self.cursor.execute(query)
             self.conn.commit()
 
+            print("Successfully Modified User")
         except Exception as e:
             print(e)
 
+
+    def add_brand(self, brand_name):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+
+    def modify_brand(self, brand_id, **data):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+    
+
+    def add_event_type(self, event_type_name):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+    
+    def modify_event_type(self, event_type_id, **data):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+    
+    def add_user_session(self, user_session_id, **data):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+    
+    def modify_user_session(self, user_session_id, **data):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+    
+    def getUserSession_between_date_exists(self, user_id, start_date, end_date) -> bool:
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+    
+    def add_user_activity(self, user_id, **data):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+    
+    def modify_user_activity(self, user_id, **data):
+        try:
+            pass
+        except Exception as e:
+            print(e)
